2db01f22817a0cd90d6705a1f6145ed9306930c6
cat12079801 <cat12079801@gmail.com>
2015-02-21 12:01:55 +0900

    本来やるべきではないけどmigrationファイルを変更

--- db/migrate/20150219074243_create_plans.rb
+++ db/migrate/20150219074243_create_plans.rb
@@ -1,16 +1,16 @@
 class CreatePlans < ActiveRecord::Migration
   def change
     create_table :plans do |t|
-      t.string :cookie
+      t.string :cookie, null: false
       t.string :password
-      t.string :title
+      t.string :title, null: false
       t.text :explanation
-      t.string :hash
-      t.date :day_start
-      t.date :day_end
+      t.string :hash, null: false
+      t.date :day_start, null: false
+      t.date :day_end, null: false
       t.text :day_out
-      t.int :day_number
-      t.boolean :enable_time
+      t.integer :day_number, null: false
+      t.boolean :enable_time, null: false
       t.time :time_start
       t.time :time_end
 
--- db/migrate/20150219074328_create_opinions.rb
+++ db/migrate/20150219074328_create_opinions.rb
@@ -1,11 +1,11 @@
 class CreateOpinions < ActiveRecord::Migration
   def change
     create_table :opinions do |t|
-      t.int :plan_id
-      t.string :cookie
+      t.integer :plan_id, null: false
+      t.string :cookie, null: false
       t.string :password
-      t.string :name
-      t.text :opinion
+      t.string :name, null: false
+      t.text :opinion, null: false
       t.text :note
 
       t.timestamps null: false
